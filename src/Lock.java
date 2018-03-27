651b7674100353f83327d7a3e3443543572e8958
estrauss35 <ems394@nau.edu>
2018-03-26 18:43:30 -0700

    Some updates on the Lock Class

--- src/Lock.java
+++ src/Lock.java
@@ -1,19 +1,62 @@
+import java.util.stream.IntStream;
 
 public class Lock {
 
-	private Account acc;
+	private Transaction trans;
 	// Which transactions are holding the lock
 	private int[] holder_ids; 
 	private String lock_type;
 	private boolean exist_conflict; 
+	private int trans_id;
 	
-	public synchronized void acquire(int trans_id, String lock_type)
+	public synchronized void acquire(int atrans_id, String alock_type)
 	{
-		
+		while(!check_conflicts())
+		{
+			try 
+			{
+				wait();
+			}
+			catch(InterruptedException e) 
+			{
+				System.out.println("EXCEPTION");
+			}
+		}
+		if(holder_ids.length == 0)
+		{
+			holder_ids[0] = atrans_id; 
+			trans_id = atrans_id;
+			lock_type = alock_type;
+		}
+		else if(holder_ids.length > 0) 
+		{
+			if(!IntStream.of(holder_ids).anyMatch(x -> x == trans_id))
+			{
+				holder_ids[holder_ids.length-1] = trans_id;
+			}
+		}
 	}
 
-	private void check_conflicts()
+	private boolean check_conflicts()
 	{
 		
+		return false;
+	}
+	
+	private void release()
+	{
+		boolean cleared = false;
+		for(int i = 0; i < holder_ids.length; i++)
+		{
+			if(cleared)
+			{
+				holder_ids[i] = holder_ids[i+1];
+			}
+			else if(holder_ids[i] == trans_id)
+			{
+				cleared = true;
+				holder_ids[i] = (Integer) null;
+			}
+		}
 	}
 }
